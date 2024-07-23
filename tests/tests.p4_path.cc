9798fcf649d3556394c526022b3a40c3d19b15f2
peter-esik <67009121+peter-esik@users.noreply.github.com>
2024-07-23 15:33:35 +0200

    Additional test cases for P4Path

--- tests/tests.p4_path.cc
+++ tests/tests.p4_path.cc
@@ -13,11 +13,17 @@ int TestP4Path ()
 	TEST(p.GetDepotName(), "Depot");
 	TEST(p.GetParts(), std::vector<std::string> ({ "Depot", "Path", "To", "file" }));
 
+	const char* pPathWithSlashStr = "//Depot/Branch/2/";
+	P4Path p2(pPathWithSlashStr);
+	TEST(p2.GetPath(), pPathWithSlashStr);
+	TEST(p2.GetDepotName(), "Depot");
+	TEST(p2.GetParts(), std::vector<std::string> ({ "Depot", "Branch", "2" }));
+
 	{
 		bool threw = false;
 		try
 		{
-			P4Path p2("something");
+			P4Path p("something");
 		}
 		catch (const P4Path::InvalidPathException& e)
 		{
@@ -30,7 +36,20 @@ int TestP4Path ()
 
 		try
 		{
-			P4Path p3("/some/path/to/file.txt");
+			P4Path p("/some/path/to/file.txt");
+		}
+		catch (const P4Path::InvalidPathException& e)
+		{
+			threw = true;
+		}
+
+		TEST(threw, true);
+
+		threw = false;
+
+		try
+		{
+			P4Path p("");
 		}
 		catch (const P4Path::InvalidPathException& e)
 		{
