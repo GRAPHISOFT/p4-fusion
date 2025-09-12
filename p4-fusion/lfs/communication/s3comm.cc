b4fe3c24ad518656b80c693a6279ac1d0232beff
Laki Zoltán <>
2025-09-12 09:29:34 +0200

    Introduce HEAD query to not upload files already stored in the repo. Remove region definition as it is the default value in the AWS and MinIO ignores the value completely. Rename local variables according to style guide.

--- p4-fusion/lfs/communication/s3comm.cc
+++ p4-fusion/lfs/communication/s3comm.cc
@@ -4,6 +4,7 @@
 #include <aws/core/Region.h>
 #include <aws/s3/S3Client.h>
 #include <aws/s3/model/PutObjectRequest.h>
+#include <aws/s3/model/HeadObjectRequest.h>
 #include <aws/core/auth/AWSCredentials.h>
 #include <aws/s3/S3ClientConfiguration.h>
 #include <aws/core/utils/stream/PreallocatedStreamBuf.h>
@@ -29,15 +30,27 @@ Communicator::UploadResult S3Comm::UploadFile(const std::vector<char>& fileConte
     config.endpointOverride = m_ServerURL;
     config.scheme = isHttps ? Aws::Http::Scheme::HTTPS : Aws::Http::Scheme::HTTP;
     config.verifySSL = isHttps;
-    config.region = Aws::Region::US_EAST_1; // MinIO ignores region, but AWS SDK requires it
 	config.useVirtualAddressing = false;
 
-	Aws::S3::S3Client s3_client(credentials, config,
-        Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy::Never, false);
+	Aws::S3::S3Client s3Client(credentials, config,
+	Aws::Client::AWSAuthV4Signer::PayloadSigningPolicy::Never, false);
 
-	Aws::S3::Model::PutObjectRequest object_request;
-	object_request.SetBucket(m_Bucket);
-	object_request.SetKey(m_Repository + "/" + oid);
+	std::string objectKey = m_Repository + "/" + oid;
+
+	//Check if object already exists
+	Aws::S3::Model::HeadObjectRequest headRequest;
+	headRequest.SetBucket(m_Bucket);
+	headRequest.SetKey(objectKey);
+
+	auto headResult = s3Client.HeadObject(headRequest);
+	if (headResult.IsSuccess())
+	{
+		return UploadResult::AlreadyExists;
+	}
+
+	Aws::S3::Model::PutObjectRequest objectRequest;
+	objectRequest.SetBucket(m_Bucket);
+	objectRequest.SetKey(objectKey);
 
 	auto streamBuf = Aws::MakeShared<Aws::Utils::Stream::PreallocatedStreamBuf>("",
 		reinterpret_cast<unsigned char*>(const_cast<char*>(fileContents.data())), 
@@ -45,10 +58,10 @@ Communicator::UploadResult S3Comm::UploadFile(const std::vector<char>& fileConte
 	auto stream = Aws::MakeShared<Aws::IOStream>("", streamBuf.get());
     stream->seekg(0, std::ios::beg);
 
-    object_request.SetBody(stream);
+    objectRequest.SetBody(stream);
 
-	auto upload_result = s3_client.PutObject(object_request);
-	return upload_result.IsSuccess() ?
+	auto uploadResult = s3Client.PutObject(objectRequest);
+	return uploadResult.IsSuccess() ?
 		UploadResult::Uploaded :
 		UploadResult::Error;
 }
